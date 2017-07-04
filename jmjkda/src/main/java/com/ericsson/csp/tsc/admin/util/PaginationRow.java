9f13249ed487fe0904d5041d788fdc3911bbc5dd
sun-bo <sunbo1124@rayootech.com>
2017-07-04 17:19:39 +0800

    update admin

--- jmjkda/src/main/java/com/ericsson/csp/tsc/admin/util/PaginationRow.java
+++
@@ -1,7 +0,0 @@
-package com.ericsson.csp.tsc.admin.util;
-
-import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
-
-public interface PaginationRow {
-    public SimplePropertyPreFilter fetchSimplePropertyPreFilter();
-}
